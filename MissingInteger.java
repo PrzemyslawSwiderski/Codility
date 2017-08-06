2f50947db1ca0fc9917cd35233b84857e7358b33
Przemysław Świderski <przemekzgierz7@gmail.com>
2017-08-06 21:27:56 +0200

    Two Codility tasks solvations

---
+++ MissingInteger.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] A) {
+        int num = 1;
+        HashSet<Integer> hashset = new HashSet<Integer>();
+
+        for (int i = 0 ; i < A.length; i++) {
+            hashset.add(A[i]);          
+
+            while (hashset.contains(num)) {
+                num++;
+            }
+        }
+
+        return num;
+    }
+}
