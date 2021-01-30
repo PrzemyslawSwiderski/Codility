511beb638030df079edd996ea3ef0ce7440b54c9
PrzemyslawSwiderski <przemekzgierz7@gmail.com>
2021-01-30 23:13:25 +0100

    New Solutions commit.

---
+++ main/PrimeChecker.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.lang.System.in;
+
+class Prime {
+
+    // Check method should list prime numbers which are contained in arguments
+    public void checkPrime(int... args) {
+        for (int i : args) {
+            if (i == 1) continue;
+            if (i == 2) {
+                System.out.print("2 ");
+                continue;
+            }
+
+            boolean isPrime = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
+
+public class PrimeChecker {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+            Prime ob = new Prime();
+            ob.checkPrime(n1);
+            ob.checkPrime(n1, n2);
+            ob.checkPrime(n1, n2, n3);
+            ob.checkPrime(n1, n2, n3, n4, n5);
+            Method[] methods = Prime.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            for (int i = 0; i < methods.length; i++) {
+                if (set.contains(methods[i].getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(methods[i].getName());
+
+            }
+            if (overload) {
+                throw new Exception("Overloading not allowed");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
